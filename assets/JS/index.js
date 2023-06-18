075e65e8f6697b0a3066ca84cd7175134c99f92c
Alolii <135501680+Alolii@users.noreply.github.com>
2023-06-18 16:08:34 -0600

    Add files via upload

---
+++ assets/JS/index.js
@@ -0,0 +1,40 @@
+let listaBotones = document.querySelectorAll("ul li a");
+let imagen = document.querySelector(".item-right img");
+
+for(let i = 0; i < listaBotones.length; i++){
+    listaBotones[i].addEventListener("mouseover", () => {
+        if (listaBotones[i].textContent === "My Work"){
+            imagen.style.opacity = "0";
+            setTimeout(() => {
+                imagen.setAttribute("src","./assets/img/my_work.jpg");
+                imagen.style.opacity = "1";
+            }, 200);
+        }
+        else if (listaBotones[i].textContent === "My Career"){
+            imagen.style.opacity = "0";
+            setTimeout(() => {
+                imagen.setAttribute("src","./assets/img/my_career.jpg");
+                imagen.style.opacity = "1";
+            }, 200);
+        }
+        else if (listaBotones[i].textContent === "About Me"){
+            imagen.style.opacity = "0";
+            setTimeout(() => {
+                imagen.setAttribute("src","./assets/img/about_me.jpg");
+                imagen.style.opacity = "1";
+            }, 200);
+        }
+        else{
+            alert("El valor del boton cambio");
+        }
+    });
+
+    listaBotones[i].addEventListener("mouseout", () => {
+        imagen.style.opacity = "0";
+        setTimeout(() => {
+            imagen.setAttribute("src","./assets/img/web-design-2038872_1280.jpg");
+            imagen.style.opacity = "1";
+        }, 200);
+    });
+}
+
